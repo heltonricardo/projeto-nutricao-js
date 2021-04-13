e60741b198ef1a7fa6afb8cd7a9c80e7c97e78e6
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-04-13 13:57:48 -0300

    Update principal.js

--- files/js/principal.js
+++ files/js/principal.js
@@ -13,35 +13,42 @@ document.querySelector("title").textContent = titulo.textContent;
 var pacientes = document.querySelectorAll(".paciente");
 
 for (var i = 0; i < pacientes.length; ++i) {
-  var paciente = pacientes[i];
+   var paciente = pacientes[i];
 
-  var peso = paciente.querySelector(".info-peso").textContent;
-  var altura = paciente.querySelector(".info-altura").textContent;
+   var peso = paciente.querySelector(".info-peso").textContent;
+   var altura = paciente.querySelector(".info-altura").textContent;
 
-  if (0 < peso && peso < 1000 && 0 < altura && altura < 3) {
-    var imc = peso / (altura * altura);
-    paciente.querySelector(".info-imc").textContent = imc.toFixed(2);
-  } else {
-    // Não é recomendável trocar os valores diretamente
-    // paciente.style.color = "white"
-    // paciente.style.backgroundColor = "lightcoral"
-    // É recomendável manipular as classes do elemento:
-    paciente.classList.add("paciente-invalido");
+   if (0 < peso && peso < 1000 && 0 < altura && altura < 3) {
+      var imc = peso / (altura * altura);
+      paciente.querySelector(".info-imc").textContent = imc.toFixed(2);
+   } else {
+      // Não é recomendável trocar os valores diretamente
+      // paciente.style.color = "white"
+      // paciente.style.backgroundColor = "lightcoral"
+      // É recomendável manipular as classes do elemento:
+      paciente.classList.add("paciente-invalido");
 
-    paciente.querySelector(".info-imc").textContent = "Dados Inválidos!";
-  }
+      paciente.querySelector(".info-imc").textContent = "Dados Inválidos!";
+   }
 }
 
 // Adição de evento de click com chamada de função comum:
 function mostraMsg() {
-    console.log("Teste de click!");
+   console.log("Teste de click!");
 }
 titulo.addEventListener("click", mostraMsg);
 
-var botao = document.querySelector("#adicionar-paciente")
+var botao = document.querySelector("#adicionar-paciente");
 
 // Adição de evento de click com chamada de função anônima:
 botao.addEventListener("click", function (event) {
-    event.preventDefault(); // Impede a atualização da página
-    alert("Teste de click!");
+   event.preventDefault(); // Impede a atualização da página
+
+   var form = document.querySelector("#form-adiciona");
+   var nome = form.nome.value;
+   var peso = form.peso.value;
+   var altura = form.altura.value;
+   var gordura = form.gordura.value;
 });
+// Quando existe o "name" nos elementos de um form é possível acessá-los
+// usando: form.nomeCampo
