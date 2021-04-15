9c6bd2b117d92eb5a47c0ac5a9ff51ca13bb85d2
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-04-15 12:50:25 -0300

    Update form.js

--- files/js/form.js
+++ files/js/form.js
@@ -37,6 +37,42 @@ function montaTr(paciente) {
    return pacienteTr;
 }
 
+function exibeErros(erros) {
+   var ul = document.querySelector("#mensagem-erro");
+   erros.forEach(function(erro) {
+      var li = document.createElement("li");
+      li.textContent = erro;
+      ul.appendChild(li);
+   });
+}
+
+function pacienteValido(paciente) {
+   var erros = [];
+
+   if (paciente.nome.trim().length == 0) {
+      erros.push("Nome inválido!");
+   }
+
+   if (!pesoValido(paciente.peso)) {
+      erros.push("Valor inválido para o peso!");
+   }
+
+   if (!alturaValida(paciente.altura)) {
+      erros.push("Valor inválido para a altura!");
+   }
+
+   if (paciente.gordura.length == 0) {
+      erros.push("Valor inválido para a gordura!");
+   }
+
+   if (erros.length) {
+      exibeErros(erros);
+      return false;
+   }
+
+   return true;
+}
+
 var botao = document.querySelector("#adicionar-paciente");
 
 // Adição de evento de click com chamada de função anônima:
@@ -45,15 +81,20 @@ botao.addEventListener("click", function (event) {
    // (atualizar página e limpar formulário )
    event.preventDefault();
 
+   var ul = document.querySelector("ul");
+   ul.innerHTML = ""; // Apaga o que tem dentro da UL de erros
+
    var form = document.querySelector("#form-adiciona");
    var paciente = obtemPacienteDoFormulario(form);
 
-   pacienteTr = montaTr(paciente);
+   if (pacienteValido(paciente)) {
+      pacienteTr = montaTr(paciente);
 
-   var tabela = document.querySelector("#tabela-pacientes");
+      var tabela = document.querySelector("#tabela-pacientes");
 
-   //Adiciona a linha na tabela:
-   tabela.appendChild(pacienteTr);
+      //Adiciona a linha na tabela:
+      tabela.appendChild(pacienteTr);
 
-   form.reset(); // Limpa os campos do formulário
+      form.reset(); // Limpa os campos do formulário
+   }
 });
